fc48dc8727f9f1a0f1a383ee215545358c45bc8c
abhikoli1996 <abhikoli1996@gmail.com>
2022-11-03 11:51:18 +0530

    updated array and local storage Assignment
    updated array and local storage Assignment.

---
+++ 10_array-assignment.js
@@ -0,0 +1,12 @@
+console.log("---------------Assignment- Array for removing duplicate elements---------------");
+let arrayOfNames=["Kamat", "Memon", "Nashpati", "Taimur", "Menon", "Kamat", "Andy", "Taimur"];
+console.log(`Given Array: ${arrayOfNames}`);
+console.log("---------------------B.Array after removing Duplicate Elements-----------------");
+let uniqueArray=[...new Set(arrayOfNames)] ;
+console.log(`${uniqueArray}`);
+console.log("---------------------C. Count of Duplicate Element-------------------------");
+let len=arrayOfNames.length-uniqueArray.length;
+console.log(`${len}`);
+console.log("---------------------D. Total Number of Unique Element---------------------");
+console.log(uniqueArray.length);
+
