0ffa7a8b73c8314f8dc6a74c8990fc5e8ca151c4
abhikoli1996 <abhikoli1996@gmail.com>
2022-11-04 12:31:21 +0530

    rename
    rename

--- 10_array-assignment.js
+++ 10_array-assignment.js
@@ -1,11 +1,11 @@
 console.log("---------------Assignment- Array for removing duplicate elements---------------");
-let arrayOfNames=["Kamat", "Memon", "Nashpati", "Taimur", "Menon", "Kamat", "Andy", "Taimur"];
+let arrayOfNames = ["Kamat", "Memon", "Nashpati", "Taimur", "Menon", "Kamat", "Andy", "Taimur"];
 console.log(`Given Array: ${arrayOfNames}`);
 console.log("---------------------B.Array after removing Duplicate Elements-----------------");
-let uniqueArray=[...new Set(arrayOfNames)] ;
+let uniqueArray = [...new Set(arrayOfNames)];
 console.log(`${uniqueArray}`);
 console.log("---------------------C. Count of Duplicate Element-------------------------");
-let len=arrayOfNames.length-uniqueArray.length;
+let len = arrayOfNames.length - uniqueArray.length;
 console.log(`${len}`);
 console.log("---------------------D. Total Number of Unique Element---------------------");
 console.log(uniqueArray.length);
